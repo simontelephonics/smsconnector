e9558da214b93f5c06707ea5bbe68ebab69bd9d1
Bill Simon <billsimon@users.noreply.github.com>
2023-03-24 15:09:55 -0400

    Adds support for Twilio Reworks some form logic Fixes module.xml manifest to include sms dependency (closes #1)

--- adaptor/Smsconnector.class.php
+++ adaptor/Smsconnector.class.php
@@ -82,6 +82,22 @@ class Smsconnector extends \FreePBX\modules\Sms\AdaptorBase {
                 );
                 $this->sendFlowroute($provider, $req, $retval['id']);
                 break;
+
+            case 'twilio':
+                // this manual generation of the www-form-data request is because Twilio wants
+                // MediaUrl specified multiple times in the request data, not as a MediaUrl[] array
+                $req = array(
+                    'From=' . urlencode("+$from"),
+                    'To=' . urlencode("+$to")
+                );
+                foreach ($media_urls as $media_url) {
+                    $req[] = 'MediaUrl=' . urlencode($media_url);
+                }
+                if ($message) {
+                    $req[] = 'Body=' . urlencode($message);
+                }
+                $this->sendTwilio($provider, implode('&', $req), $retval['id']);
+                break;
         }
         
         return $retval;
@@ -127,6 +143,15 @@ class Smsconnector extends \FreePBX\modules\Sms\AdaptorBase {
                 );
                 $this->sendFlowroute($provider, $req, $retval['id']);
                 break;
+
+            case 'twilio':
+                $req = array(
+                    'From' => '+'.$from,
+                    'To' => '+'.$to,
+                    'Body' => $message
+                );
+                $this->sendTwilio($provider, $req, $retval['id']);
+                break;
         }
 
         return $retval;
@@ -152,6 +177,19 @@ class Smsconnector extends \FreePBX\modules\Sms\AdaptorBase {
         try {
             $flowrouteResponse = $session->post('', $headers, $payload, $options);
             freepbx_log(FPBX_LOG_INFO, $flowrouteResponse->body, true);
+            $this->setDelivered($mid);
+        } catch (\Exception $e) {
+            throw new \Exception('Unable to send message: ' .$e->getMessage());
+        }
+    }
+
+    private function sendTwilio($provider, $payload, $mid) {
+        $options = array("auth" => array($provider['api_key'], $provider['api_secret']));
+        $url = 'https://api.twilio.com/2010-04-01/Accounts/' . $provider['api_key'] . '/Messages.json';
+        $session = \FreePBX::Curl()->requests($url);
+        try {
+            $twilioResponse = $session->post('', null, $payload, $options);
+            freepbx_log(FPBX_LOG_INFO, $twilioResponse->body, true);
             $this->setDelivered($mid);
         } catch (\Exception $e) {
             throw new \Exception('Unable to send message: ' .$e->getMessage());
---
+++ public/twilio.php
@@ -0,0 +1,54 @@
+<?php
+
+if (empty($_SERVER['HTTP_X_TWILIO_SIGNATURE'])) {
+    exit;
+}
+
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
+    $postdata = $_POST;
+ 
+    $to = ltrim($postdata['To'], '+');
+    $from = ltrim($postdata['From'], '+');
+    $text = $postdata['Body'];
+    $emid = $postdata['SmsMessageSid'];
+
+    // load FreePBX
+    $bootstrap_settings['freepbx_auth'] = false;
+    require '/etc/freepbx.conf';
+    $freepbx = \FreePBX::Create();
+    $connector = $freepbx->Sms->loadAdaptor('Smsconnector');
+    
+    try {
+        $msgid = $connector->getMessage($to, $from, '', $text, null, null, $emid);
+    } catch (\Exception $e) {
+        http_response_code(500);
+        throw new \Exception('Unable to get message: ' .$e->getMessage());
+    }
+    
+    if (isset($postdata['NumMedia']) && ($postdata['NumMedia'] > 0)) {
+        for ($x=0;$x<=$postdata['NumMedia'];$x++) {
+            $img = file_get_contents($postdata["MediaUrl$x"]);
+            foreach ($http_response_header as $header) {
+                if (preg_match('/Content-Disposition.*filename="(.*)"/', $header, $matches)) {
+                    $name = $matches[1];
+                }
+            }
+
+            try {
+                $connector->addMedia($msgid, $name, $img);
+            } catch (\Exception $e) {
+                http_response_code(500);
+                throw new \Exception('Unable to store MMS media: ' .$e->getMessage());
+            }
+        }
+    }
+            
+    $connector->emitSmsInboundUserEvt($msgid, $to, $from, '', $text, null, 'Smsconnector', $emid);
+
+    header('Content-Type: application/xml');
+    echo '<Response/>';
+
+} else {
+    http_response_code(405);
+}
+
