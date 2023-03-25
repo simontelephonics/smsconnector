ae408c3430a6fab36b6d98a9f96d765f26d41905
Javier Pastor <jpastor@cerebelum.net>
2023-03-25 22:08:39 +0100

    add kvstore to backaup and restroe.

--- Backup.php
+++ Backup.php
@@ -1,10 +1,15 @@
 <?php
 namespace FreePBX\modules\Smsconnector;
 use FreePBX\modules\Backup as Base;
-class Backup Extends Base\BackupBase{
-	public function runBackup($id,$transaction){
+class Backup Extends Base\BackupBase
+{
+	public function runBackup($id,$transaction)
+	{
 		$this->addDependency('sms');
-		$configs = $this->dumpTables();
+		$configs = [
+			'kvstore' => $this->dumpKVStore(),
+			'tables'  => $this->dumpTables(),
+		];
 		$this->addConfigs($configs);
 	}
 }
